646a40d710d68b7d23e33eb8dec0d59764c252a6
hugobonforte <hugobonforte@gmail.com>
2026-05-14 15:48:54 -0600

    Add new routes

--- flask_test.py
+++ flask_test.py
@@ -1,5 +1,6 @@
+import os
 import random
-from flask import Flask
+from flask import Flask, send_file
 from datos import residuos
 
 app = Flask(__name__)
@@ -21,7 +22,39 @@ def home():
     return (
         f'<a href="/random_fact">¡Ver un dato aleatorio!</a>'
         f'<a href="/lista_de_cosas_organicas_reciclables_o_basura">¡Ver una lista de cosas orgánicas, reciclables o basura!</a>'
+        f'<a href="/meme_aleatorio_de_python">¡Ver un meme aleatorio de Python!</a>'
+        f'<a href="/Generador_de_contraseñas_aleatorias">¡Generar una contraseña aleatoria!</a>'
+        f'<a href="/adivinanzas">¡Ver adivinanzas!</a>'
     )
+    
+@app.route("/adivinanzas")
+def juego_adivinanzas():
+    # Guardamos las preguntas y respuestas en una lista de diccionarios
+    lista_adivinanzas = [
+        {"pregunta": "¿Cómo se llama el miedo a la llegada de Papá Noel?", "respuesta": "Claustrofobia"},
+        {"pregunta": "Tengo cola y cabeza, pero no cuerpo. ¿Qué soy?", "respuesta": "Una moneda"},
+        {"pregunta": "¿Cuál es la estrella que no tiene luz?", "respuesta": "La estrella de mar"},
+        {"pregunta": "Tengo agujas y no sé coser, tengo números y no sé leer. ¿Qué soy?", "respuesta": "Un reloj"},
+        {"pregunta": "No muerde ni ladra, pero tiene dientes y la casa guarda. ¿Qué es?", "respuesta": "Una llave"},
+        {"pregunta": "¿Cuál es la estrella que no tiene luz?", "respuesta": "La estrella de mar"}   
+    ]
+    seleccionada = random.choice(lista_adivinanzas)
+    return f"Pregunta: {seleccionada['pregunta']} <br> Respuesta: {seleccionada['respuesta']}"
+
+@app.route("/Generador_de_contraseñas_aleatorias")
+def password_generator():
+    character = "+/*!?abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    longitud = 12  # Fixed length for web route
+    contraseña = ""
+    for _ in range(longitud):
+        contraseña += random.choice(character)
+    return f"Contraseña generada: {contraseña}"
+
+@app.route("/meme_aleatorio_de_python")
+def memes():
+    imagenes = os.listdir('images')
+    imagen = random.choice(imagenes)
+    return send_file(os.path.join("images", imagen))
     
 
 @app.route("/lista_de_cosas_organicas_reciclables_o_basura")
@@ -32,7 +65,7 @@ def lista_residuos():
     
     return f'<p>El residuo <strong>{objeto}</strong> es de tipo: <strong>{tipo}</strong></p>'
 
-@app.route("/secret!")
+@app.route("/secret")
 def pagina_secreta():
     # randint(1, 2) elige un número entero aleatorio entre 1 y 2
     number = random.randint(1, 2)
